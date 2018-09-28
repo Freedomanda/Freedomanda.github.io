c3611844859be382146563b33d120abb1ea6af91
C5275218 <changchun.wu01@sap.com>
2018-09-28 16:10:01 +0800

    Refactor
    Add embed view and fragment

---
+++ controller/HomePage/Panel2.controller.js
@@ -0,0 +1,10 @@
+﻿sap.ui.define([ "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+   /* "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel" */
+    ],
+    function (Controller, messageToast) {
+    "use strict";
+        return Controller.extend("chuntian.home.controller.HomePage.Panel2", {
+    });
+});
---
+++ controller/HomePage/Panel3.controller.js
@@ -0,0 +1,23 @@
+﻿sap.ui.define([ "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+   /* "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel" */
+    ],
+    function (Controller, messageToast) {
+    "use strict";
+        return Controller.extend("chuntian.home.controller.HomePage.Panel3", {
+        onShowHello: function () {
+            // show a native JavaScript alert static
+            //messageToast.show("点我干哈");
+            //read msg from i18n
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            // show message
+            messageToast.show(sMsg);
+            },
+            onOpenDialog: function () {
+
+            }
+    });
+});
