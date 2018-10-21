d16d34449c1bac82f6414e4d99ac83fa6e0acad9
C5275218 <changchun.wu01@sap.com>
2018-10-21 17:29:24 +0800

    Filter

---
+++ Chunge_UI5/PracticsHome/controller/PracticeList.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+    "chuntian/PracticeHome/model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function(Controller, formatter,Filter,FilterOperator) {
+	"use strict";
+
+	return Controller.extend("chuntian.PracticeHome.controller.PracticeList", {
+
+		formatter: formatter,
+
+		onInit: function () {
+
+        },
+        onFilterPractice: function (oEvent) {
+            //Build filter array
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("Name",FilterOperator.Contains,sQuery));
+            }
+            //filter binding
+            var oList = this.byId("PracticeList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+        }
+	});
+});
--- Chunge_UI5/PracticsHome/model/formatter.js
+++ Chunge_UI5/PracticsHome/model/formatter.js
@@ -1,6 +1,18 @@
 sap.ui.define([], function () {
 	"use strict";
 	return {
-
+        statusText: function (sStatus) {
+            var resourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            switch (sStatus) {
+                case "A":
+                    return resourceBundle.getText("practiceStatusA");
+                case "B":
+                    return resourceBundle.getText("practiceStatusB");
+                case "C":
+                    return resourceBundle.getText("practiceStatusC");
+                default:
+                    return sStatus;
+            }
+        }
 	};
 });
