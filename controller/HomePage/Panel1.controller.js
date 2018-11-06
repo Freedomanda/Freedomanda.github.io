b3f984a6ada47b275444c593eb5ad6faa1957332
C5275218 <changchun.wu01@sap.com>
2018-11-06 17:54:12 +0800

    Update hireracy

--- controller/HomePage/Panel1.controller.js
+++ controller/HomePage/Panel1.controller.js
@@ -1,11 +1,18 @@
 ﻿sap.ui.define([ "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/routing/History"
    /* "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel" */
     ],
-    function (Controller, messageToast) {
+    function (Controller, messageToast,History) {
     "use strict";
         return Controller.extend("chuntian.home.controller.Homepage.Panel1", {
-        
+            getRouter: function () {
+                return sap.ui.core.UIComponent.getRouterFor(this);
+            },
+            onPress1: function () {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("project_summary");
+            }
     });
 });
