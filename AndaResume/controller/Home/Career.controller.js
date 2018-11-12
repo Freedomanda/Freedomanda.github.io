3c3b9f0a765a97509aaf25cf235c402a6aee18d4
C5275218 <changchun.wu01@sap.com>
2018-11-12 18:14:34 +0800

    update career

--- AndaResume/controller/Home/Career.controller.js
+++ AndaResume/controller/Home/Career.controller.js
@@ -4,8 +4,8 @@
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/ui/core/routing/History",
-    "sap/m/MessageToast"
-], function (Controller, formatter, Filter, FilterOperator, History, MessageToast) {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"], function (Controller, formatter, Filter, FilterOperator, History, JSONModel, MessageToast ) {
     "use strict";
 
     return Controller.extend("chuntian.resume.controller.Home.Career", {
@@ -13,11 +13,51 @@
         formatter: formatter,
 
         onInit: function () {
+            var dateFrom = new Date();
+            dateFrom.setUTCDate(2);
+            dateFrom.setUTCMonth(1);
+            dateFrom.setUTCFullYear(2014);
 
+            var dateTo = new Date();
+            dateTo.setUTCDate(17);
+            dateTo.setUTCMonth(1);
+            dateTo.setUTCFullYear(2014);
+
+            var oModel = new JSONModel();
+            oModel.setData({
+                delimiterDRS1: "-",
+                dateValueDRS1: dateFrom,
+                secondDateValueDRS1: dateTo,
+                dateFormatDRS1: "yyyy/MM/dd",
+                dateValueDRS2: new Date(2016, 1, 16),
+                secondDateValueDRS2: new Date(2016, 1, 18),
+                dateMinDRS2: new Date(2016, 0, 1),
+                dateMaxDRS2: new Date(2016, 11, 31)
+            });
+            this.getView().setModel(oModel);
+
+            this._iEvent = 0;
         },
         onPress: function (oEvent) {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.navTo("helloworld");
+        },
+        handleChange: function (oEvent) {
+            var sFrom = oEvent.getParameter("from");
+            var sTo = oEvent.getParameter("to");
+            var bValid = oEvent.getParameter("valid");
+            var oEventSource = oEvent.getSource();
+
+            this._iEvent++;
+
+            var oText = this.byId("TextEvent");
+            oText.setText("Id: " + oEventSource.getId() + "\nFrom: " + sFrom + "\nTo: " + sTo);
+
+            if (bValid) {
+                oEventSource.setValueState(sap.ui.core.ValueState.None);
+            } else {
+                oEventSource.setValueState(sap.ui.core.ValueState.Error);
+            }
         },
         onNavBack: function (oEvent) {
             var oHistory, sPreviousHash;
@@ -27,6 +67,30 @@
                 window.history.go(-1);
             } else {
                 this.getRouter().navTo("resume", {}, true /*no history*/);
+            }
+        },
+        onSelectionChange: function (oEvent) {
+            //the selected item could be found via the "item" parameter of "selectionChange" event
+            //sap.m.MessageToast.show("oEvent.getParameter('item').getText(): " + oEvent.getParameter("item").getText() + " selected");
+            //the selected item could also be found via the "selectItem" association not only when "selectionChange" but when needed
+            /*this.byId('CampanyName').setText("SegmentedButton" + "getSelectedItem(): " +
+                sap.ui.getCore().byId(this.byId('Company').getSelectedItem()).getText());*/
+            this.byId('CampanyName').setText( sap.ui.getCore().byId(this.byId('Company').getSelectedItem()).getText() );
+            switch (sap.ui.getCore().byId(this.byId('Company').getSelectedItem()).getText()){
+                case "Capgemini":
+                    this.byId('Position').setText("SAP ABAP consultant"); break;
+                    this.byId('desc').setValue("The description of Capgemini"); break;
+                case "Huashi":
+                    this.byId('Position').setText("SAP ABAP Team leader"); break;
+                    this.byId('desc').setValue("The description of Huashi"); break;
+                case "Mingsi":
+                    this.byId('Position').setText("SAP ABAP consultant"); break;
+                    this.byId('desc').setValue("The description of Mingsi"); break;
+                case "Hand":
+                    this.byId('Position').setText("ERP development engineer"); break;
+                    this.byId('desc').setValue("The description of Hand"); break;
+                default:
+                    this.byId('Position').setText("");
             }
         },
         onShowHello: function () {
